b87891482cef1ed8a72bef2c9c8b773f511ab600
Alexandre Borlini <borlinialexandre@gmail.com>
2020-10-02 15:20:31 -0300

    Initial code

---
+++ public/client.js
@@ -0,0 +1,82 @@
+const socket = io('http://localhost:3000');
+
+
+// Tela inicial
+var inputPlayerName;
+var buttonEnterGame;
+
+
+// Dados do jogador
+var playerName;
+var playerSpeed = 5;
+var playerConnection;
+ 
+
+// RENDERIZAR ---------------------------------------------------------------------------------------------------------------------------
+
+function setup() { // Start
+
+    createCanvas(800, 800);
+
+    initialUI();
+}
+  
+function draw() { // Update
+
+    background('#333333');
+}
+
+
+// JANELA INICIAL ------------------------------------------------------------------------------------------------------------------------------------
+
+// Setar janela inicial
+function initialUI(){
+
+    setInputPlayerName();
+    setButtonEnterGame();
+}
+
+
+function setInputPlayerName(){ // Input nome do player 
+
+    inputPlayerName = createInput();
+    inputPlayerName.size(300, 30);
+    inputPlayerName.position(250, 400);
+}
+
+
+function setButtonEnterGame(){ // Botão de entraqr no jogo
+
+    buttonEnterGame = createButton('ENTRAR');
+    buttonEnterGame.size(100, 50);
+    buttonEnterGame.position(350, 450);
+
+    buttonEnterGame.mousePressed(enterGame);
+}
+
+
+function enterGame(){ // Entrar no jogo
+
+    playerName = inputPlayerName.value();
+
+    var playerData = {
+
+        name: playerName
+    }
+
+    socket.emit('enterGame', playerData);
+}
+
+
+function deleteInitialUI(){ // Apagar a UI inicial 
+
+    buttonEnterGame.remove();
+    inputPlayerName.remove();
+}
+
+
+// Conexões com servidor
+
+
+
+// JOGO ----------------------------------------------------------------------------------------------------------------------------------------------
